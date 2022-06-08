460233e05a75b451775395089ad9990155cfc1c4
ZhengTaoLu <ZhengTaoL@users.noreply.github.com>
2022-06-08 19:11:49 +0300

    2nd try

---
+++ Currency_Exchange.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Currency_Exchange {
+    public static void main(String[] args) throws Exception {
+        HashMap<Integer, String> Rahaa = new HashMap<>();
+        Scanner lukija = new Scanner(System.in);
+
+        Rahaa.put(1, "EUR");
+        Rahaa.put(2, "USD");
+        Rahaa.put(3, "CNY");
+        Rahaa.put(4, "JPY");
+        Rahaa.put(5, "TWD");
+
+        String fromCode, toCode;
+        double määrä;
+    }
+}
