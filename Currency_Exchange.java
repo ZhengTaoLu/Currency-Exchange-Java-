596dd1ddb2f535dc3e71a2e9a6ae1a20fdb9d448
ZhengTaoLu <56450594+ZhengTaoLu@users.noreply.github.com>
2022-06-08 17:54:13 +0300

    Add files via upload

---
+++ Currency_Exchange.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+public class Currency_Exchange {
+    public static void main(String[] args) throws Exception {
+        HashMap<Integer, String> Rahaa = new HashMap<>();
+
+        Rahaa.put(1, "EUR");
+    }
+}
