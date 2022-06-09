d556ac7d4002149324fd0213ca673c3855d80b86
ZhengTaoLu <ZhengTaoL@users.noreply.github.com>
2022-06-09 16:12:08 +0300

    4rth try

--- Currency_Exchange.java
+++ Currency_Exchange.java
@@ -1,22 +1,47 @@
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.Scanner;
 
 public class Currency_Exchange {
     public static void main(String[] args) throws Exception {
         HashMap<Integer, String> Rahaa = new HashMap<>();
-        Scanner lukija = new Scanner(System.in);
+        try (Scanner lukija = new Scanner(System.in)) {
+            Rahaa.put(1, "EUR");
+            Rahaa.put(2, "USD");
+            Rahaa.put(3, "CNY");
+            Rahaa.put(4, "JPY");
+            Rahaa.put(5, "TWD");
 
-        Rahaa.put(1, "EUR");
-        Rahaa.put(2, "USD");
-        Rahaa.put(3, "CNY");
-        Rahaa.put(4, "JPY");
-        Rahaa.put(5, "TWD");
+            String mistä, minne;
+            double määrä;
 
-        String fromCode, toCode;
-        double määrä;
+            System.out.println("Currency converting from? \n");
+            System.out.println("1:EUR \t 2:USD \t 3:CNY \t " +
+                                "4:JPY \t 5:TWD \n");
+            
+            mistä = Rahaa.get(Integer.valueOf(lukija.nextLine()));
 
-        System.out.println("Currency converting from?" + "\n");
-        System.out.println("1:EUR \t 2:USD \t 3:CNY \t " +
-                            "4:JPY \t 5:TWD" + "\n");
+            System.out.println("Currency converting to? \n");
+            System.out.println("1:EUR \t 2:USD \t 3:CNY \t " +
+                                "4:JPY \t 5:TWD \n");
+            
+            minne = Rahaa.get(Integer.valueOf(lukija.nextLine()));
+
+            System.out.println("Amount? \n");
+            määrä = Double.valueOf(lukija.nextLine());
+        }
+
+
+    }
+    private static void Valuutta(String mistä, String minne, Double määrä) throws IOException{
+        
+        String url_str = "https://api.exchangerate.host/convert?from=" + mistä +"&to=" + minne;
+
+        URL url = new URL(url_str);
+        HttpURLConnection request = (HttpURLConnection) url.openConnection();
+        request.setRequestMethod("GET");
+        
     }
 }
