256ad96efef659cf6395c9e6ff4329cf63d74f72
ZhengTaoLu <ZhengTaoL@users.noreply.github.com>
2022-06-11 14:12:49 +0300

    6th leg

--- Currency_Exchange.java
+++ Currency_Exchange.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.HashMap;
@@ -21,12 +23,14 @@ public class Currency_Exchange {
             System.out.println("1:EUR \t 2:USD \t 3:CNY \t " +
                                 "4:JPY \t 5:TWD \n");
             
+            System.out.print("Here: ");
             mistä = Rahaa.get(Integer.valueOf(lukija.nextLine()));
 
             System.out.println("Currency converting to? \n");
             System.out.println("1:EUR \t 2:USD \t 3:CNY \t " +
                                 "4:JPY \t 5:TWD \n");
             
+            System.out.print("Here: ");                    
             minne = Rahaa.get(Integer.valueOf(lukija.nextLine()));
 
             System.out.println("Amount? \n");
@@ -38,17 +42,25 @@ public class Currency_Exchange {
     private static void Valuutta(String mistä, String minne, Double määrä) throws IOException{
         
         String url_str = "https://api.exchangerate.host/convert?from=" + mistä +"&to=" + minne;
+        
         /** 
          * ! instantiate the URL object with the target URL of the resource to request.
         */
         URL url = new URL(url_str);
+        
         /** 
          * ! instantiate the HttpURLConnection with the URL object 
          * ! - A new connection is opened every time by calling the openConnection method of the protocol handler for this URL. 
          * 
          * ! 1. This is the point where the connection is opened.
          */                                             
-        HttpURLConnection request = (HttpURLConnection) url.openConnection();
+        
+        /**
+         * ! Why (HttpURLConnection) url.openConnection() insteasd of new HttpURLConnection()?
+         * https://stackoverflow.com/questions/10119067/java-httpurlconnection-class-program
+         */
+         HttpURLConnection request =  (HttpURLConnection) url.openConnection();
+        
         /**
          * ! URL class:
          * The URL class is the gateway to any of the resources available on the internet. 
@@ -60,10 +72,11 @@ public class Currency_Exchange {
          * An abstract class directly extending from URLConnection class. 
          * It includes all the functionality of its parent class with additional HTTP-specific features.
          */
-        request.setRequestMethod("GET");
+        
+         request.setRequestMethod("GET");
         
         if(request.getResponseCode() == HttpURLConnection.HTTP_OK){
-            
+            BufferedReader fel = new BufferedReader(new InputStreamReader(request.getInputStream()));
         }
     }
 }
